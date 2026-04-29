68fedc45d39fafd1e10a3d42604d78d252c4dcd4
Aishwarya Akella <aishwaryaakella.dev@gmail.com>
2026-04-29 15:09:09 +0530

    Initial Commit

---
+++ Facet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Facet
+//
+//  Created by Aishwarya on 29/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Facet/FacetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacetApp.swift
+//  Facet
+//
+//  Created by Aishwarya on 29/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FacetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
